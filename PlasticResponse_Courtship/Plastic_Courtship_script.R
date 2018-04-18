35d174a0f2b4d81f27e0ceb9f13b60ce1d494885
Paul Knoops <knoopsp@mcmaster.ca>
2018-04-18 15:47:10 -0400

    Outline of plots and figure captions

--- PlasticResponse_Courtship/Plastic_Courtship_script.R
+++ PlasticResponse_Courtship/Plastic_Courtship_script.R
@@ -99,6 +99,7 @@
           #     axis.text.x=element_blank(),
           axis.text.x= element_text(size=12.5),
           axis.ticks.x=element_blank()) +
+    scale_shape_manual(values=c(15, 17)) + 
     scale_color_manual(values=c("#999999", "#E69F00"))
   
   print(gg_courtProp2)
